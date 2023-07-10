138ebc9cea851fa6c1f4642967380ffe36b77303
coorise <team.coorise@gmail.com>
2023-07-10 02:01:08 -0700

    Swyger NodeJS Mailer added

--- storage/src/app/doc/swagger/basicInfo.js
+++ storage/src/app/doc/swagger/basicInfo.js
@@ -10,7 +10,7 @@ export default {
         version: process.env.APP_VERSION ||version,
         contact: {
             name: "@swyger/storage",
-            email: "support-storage.swyger@agglomy.com",
+            email: "swyger.support@agglomy.com",
             url: "https://swyger.agglomy.com", // your website
         },
     },
